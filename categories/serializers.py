c7d68e83f3adeda051f08d10c2416af5b78458e6
Paqimm <mcgeewilliam815@gmail.com>
2022-02-26 15:05:39 +0300

    chore (Association) : add books to their category

--- categories/serializers.py
+++ categories/serializers.py
@@ -1,7 +1,9 @@
+from books.models import Book
 from categories.models import Category
 from rest_framework import serializers
 
 class CategorySerializer(serializers.ModelSerializer):
+    books= serializers.PrimaryKeyRelatedField(many=True,queryset=Book.objects.all())
     class Meta:
         model= Category
-        fields= ["category_name"]
+        fields= ["category_name","books"]
