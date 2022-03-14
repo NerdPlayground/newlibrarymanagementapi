e13852e59e8b5b2c56ee2336cce185949cf83b49
Paqimm <mcgeewilliam815@gmail.com>
2022-03-14 12:49:02 +0300

    refractor (Book) : add required fields to books

--- books/models.py
+++ books/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 class Book(models.Model):
-    book_name= models.CharField(max_length=255)
-    book_author= models.CharField(max_length=255)
-    book_category= models.ForeignKey('categories.Category',related_name='books',on_delete=models.CASCADE)
+    isbn= models.CharField(max_length=13,unique=True)
+    category= models.ForeignKey('categories.Category',related_name='books',on_delete=models.CASCADE)
+    name= models.CharField(max_length=255)
+    description= models.TextField()
+    author= models.CharField(max_length=255)
+    published_on= models.DateField()
+    publisher= models.CharField(max_length=255)
+    language= models.CharField(max_length=255)
+    pages= models.IntegerField()
--- books/serializers.py
+++ books/serializers.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model= Book
-        fields= ["book_category","id","book_name","book_author"]
+        fields= ["isbn","category","name","description","author","published_on","publisher","language","pages"]
