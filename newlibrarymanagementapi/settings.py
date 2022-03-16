9c460cfc496edfcd2e06116675330f6460f15cf9
Paqimm <mcgeewilliam815@gmail.com>
2022-03-16 16:40:19 +0300

    chore (Server) : run server

--- newlibrarymanagementapi/settings.py
+++ newlibrarymanagementapi/settings.py
@@ -9,11 +9,10 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
-from pathlib import Path
-from datetime import timedelta
-from decouple import config
 import django_heroku
+from pathlib import Path
+from decouple import config
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
