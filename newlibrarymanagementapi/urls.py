00df1ac96c06c619a55157cf147f2432481c6486
Paqimm <mcgeewilliam815@gmail.com>
2022-03-17 23:54:58 +0300

    chore (Project) : uncomment required lines

--- newlibrarymanagementapi/urls.py
+++ newlibrarymanagementapi/urls.py
@@ -39,7 +39,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('',include('categories.urls')),
     path('',include('books.urls')),
     path('',include('authentication.urls')),
