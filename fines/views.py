90b3dfcf3084723d7d7e27df9c051b4067f51499
Paqimm <mcgeewilliam815@gmail.com>
2022-03-21 17:43:26 +0300

    refractor (Fine) : use class instance of today

--- fines/views.py
+++ fines/views.py
@@ -13,25 +13,26 @@ class UpdatePatronFinesAPIView(GenericAPIView):
     permission_classes= [IsAdminUser]
     serializer_class= [FineSerializer]
 
+    def __init__(self):
+        self.today= datetime.date.today()
+
     def get_object(self,loaned_book):
-        today= datetime.date.today()
         try:
             return (Fine.objects.get(transaction=loaned_book),True,)
         except Fine.DoesNotExist:
             fine= Fine.objects.create(
                 transaction= loaned_book,
-                amount= (today-loaned_book.due_date).days * 50,
+                amount= (self.today-loaned_book.due_date).days * 50,
             )
             return (fine,False,)
 
     def get(self,request):
-        today= datetime.date.today()
-        loaned_books= Transaction.objects.filter(returned_at=None,due_date__lt=today)
+        loaned_books= Transaction.objects.filter(returned_at=None,due_date__lt=self.today)
         for loaned_book in loaned_books:
             retrieved_fine= self.get_object(loaned_book)
             if retrieved_fine[1]:
                 fine= retrieved_fine[0]
-                fine.amount= (today-loaned_book.due_date).days * 50
+                fine.amount= (self.today-loaned_book.due_date).days * 50
                 fine.save()
         
         return Response({"message":"Library fines have been updated."},status=status.HTTP_200_OK)
