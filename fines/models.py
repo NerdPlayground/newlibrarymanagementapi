a5020e15b34378827685d7d23a7a035e3ec1dd82
Paqimm <mcgeewilliam815@gmail.com>
2022-03-21 17:39:39 +0300

    chore (Fine) : update and create fines from transactions by librarian librarian view all available fines patrons view their own fines

--- fines/models.py
+++ fines/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 
 class Fine(models.Model):
-    created_at= models.DateTimeField(auto_now_add=True)
+    created_at= models.DateField(auto_now_add=True)
     transaction= models.OneToOneField('transactions.Transaction',related_name='transaction',on_delete=models.DO_NOTHING)
     amount= models.IntegerField()
     paid_on= models.DateField(null=True,blank=True)
--- fines/views.py
+++ fines/views.py
@@ -5,7 +5,6 @@ from rest_framework import status
 from students.models import Student
 from transactions.models import Transaction
 from fines.serializers import FineSerializer
-from django.contrib.auth import authenticate
 from rest_framework.response import Response
 from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import IsAdminUser,IsAuthenticated
@@ -14,75 +13,33 @@ class UpdatePatronFinesAPIView(GenericAPIView):
     permission_classes= [IsAdminUser]
     serializer_class= [FineSerializer]
 
+    def get_object(self,loaned_book):
+        today= datetime.date.today()
+        try:
+            return (Fine.objects.get(transaction=loaned_book),True,)
+        except Fine.DoesNotExist:
+            fine= Fine.objects.create(
+                transaction= loaned_book,
+                amount= (today-loaned_book.due_date).days * 50,
+            )
+            return (fine,False,)
+
     def get(self,request):
-        loaned_books= Transaction.objects.filter(returned_at=None,due_date__lt=datetime.date.today())
+        today= datetime.date.today()
+        loaned_books= Transaction.objects.filter(returned_at=None,due_date__lt=today)
         for loaned_book in loaned_books:
-            fine= Fine.objects.get_or_create(transaction= loaned_book,amount= 50)
-            if not fine[1]:
-                fine= fine[0]
-                fine.amount= (datetime.date.today() - loaned_book.due_date)*50
+            retrieved_fine= self.get_object(loaned_book)
+            if retrieved_fine[1]:
+                fine= retrieved_fine[0]
+                fine.amount= (today-loaned_book.due_date).days * 50
                 fine.save()
         
-        fines= Fine.objects.all()
-        serializer= FineSerializer(fines,many=True)
-        return Response(serializer.data,status=status.HTTP_200_OK)
-
-class UpdateFines:
-    def difference(self,before):
-        today,then= int(datetime.datetime.now().strftime("%d")),int(before.strftime("%d"))
-        this_month,last_month= int(datetime.datetime.now().strftime("%m")),int(before.strftime("%m"))
-        this_year,last_year= int(datetime.datetime.now().strftime("%Y")),int(before.strftime("%Y"))
-
-        if int(this_year != last_year):
-            days_till_end= int()
-            days_till_now= int()
-            days_of_months= [31,28,31,30,31,30,31,31,30,31,30,31]
-            if last_year%4 == 0:
-                days_of_months[1]= 29
-            
-            if last_month == 12:
-                days_till_end= 31-then
-            else:
-                for days in range(last_month,12):
-                    days_till_end += days_of_months[days]
-                days_till_end += (last_month-then)
-            
-            if this_month == 1:
-                days_till_now += today
-            else:
-                for days in range(0,this_month-1):
-                    days_till_now += days_of_months[days]
-                days_till_now += today
-            
-            return days_till_end+days_till_now
-
-        elif int(this_month != last_month):
-            last_month_days= (
-                31 if last_month in [1,3,5,7,8,10,12]
-                else 28 if last_month is 2 and last_year % 4 == 0
-                else 30
-            )
-            return last_month_days+today-then
-
-        else:
-            return int(today)-int(then)
-    
-    def upddate_fine(self,fine):
-        today= str(datetime.datetime.now()).replace(" ","T") + "Z"
-        if fine.created_at is not today:
-            fine.amount= self.difference(fine.transaction.issued_at)*50
-            fine.save()
-
-    def upddate_fines(self,fines):
-        for fine in fines:
-                self.upddate_fine(fine)
+        return Response({"message":"Library fines have been updated."},status=status.HTTP_200_OK)
 
 class FineAPIView(GenericAPIView):
     serializer_class= FineSerializer
     permission_classes= [IsAdminUser]
     def get(self,request):
-        perform_update= UpdateFines()
-        perform_update.upddate_fines(Fine.objects.all())
         fines= Fine.objects.all()
         serializer= FineSerializer(fines,many=True)
         return Response(serializer.data,status=status.HTTP_200_OK)
@@ -90,21 +47,39 @@ class FineAPIView(GenericAPIView):
 class FineDetailAPIView(GenericAPIView):
     serializer_class= FineSerializer
     permission_classes= [IsAuthenticated]
+
+    def get_object(self,transaction):
+        try:
+            return (Fine.objects.get(transaction=transaction),True,)
+        except Fine.DoesNotExist:
+            return (None,False,)
+
     def get(self,request):
         student= Student.objects.get(user=request.user)
-        perform_update= UpdateFines()
-        perform_update.upddate_fines(Fine.objects.all())
-        fines= Fine.objects.filter(student=student)
+        transactions= Transaction.objects.filter(student=student)
+        fines= list()
+        for transaction in transactions:
+            fine= self.get_object(transaction)
+            if fine[1]:
+                fines.append(fine[0])
         serializer= FineSerializer(fines,many=True)
         return Response(serializer.data,status=status.HTTP_200_OK)
 
-class PayFineAPIView(GenericAPIView):
-    permission_classes= [IsAuthenticated]
-    def post(self,request):
-        fine= Fine.objects.get(id=request.data.get('fine'))
-        amount= request.data.get('amount')
-        if amount < fine.amount:
-            return Response({"Fine":fine.amount,"Message":"Please pay the full amount"},status=status.HTTP_200_OK)
-        else:
-            fine.delete()
-            return Response({"Message":"Thank you for clearing your fine."},status=status.HTTP_200_OK)
+# class PayFineAPIView(GenericAPIView):
+#     permission_classes= [IsAuthenticated]
+#     def get_object(self,pk):
+#         try:
+#             return Fine.objects.get(pk=pk)
+#         except Fine.DoesNotExist:
+#             raise Http404
+    
+#     def post(self,request,pk):
+#         fine= self.get_object(pk=pk)
+#         amount= request.data.get('amount')
+
+#         if amount < fine.amount:
+#             return Response({"Fine":fine.amount,"Message":"Please pay the full amount"},status=status.HTTP_200_OK)
+#         else:
+#             fine.paid_on= datetime.date.today()
+#             fine.save()
+#             return Response({"Message":"Thank you for clearing your fine."},status=status.HTTP_200_OK)
