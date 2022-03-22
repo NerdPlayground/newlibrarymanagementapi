bc7b8cd99e4ea4b740331021fea4ee9fcc6041ad
Paqimm <mcgeewilliam815@gmail.com>
2022-03-22 14:04:10 +0300

    chore (BookItem) : view book item possession student the book item has been loaned to student the book item has been reserved by

--- book_items/models.py
+++ book_items/models.py
@@ -1,9 +1,29 @@
 from django.db import models
 
 class BookItem(models.Model):
-    book= models.ForeignKey('books.Book',related_name='book_items',on_delete=models.CASCADE)
+    book= models.ForeignKey(
+        'books.Book',
+        related_name='book_items',
+        on_delete=models.CASCADE
+    )
     reference= models.BooleanField(default=False)
+    loaned_to= models.ForeignKey(
+        'students.Student',
+        related_name='loaned_book_items',
+        on_delete=models.DO_NOTHING,
+        null=True,blank=True
+    )
+    reserved_by= models.ForeignKey(
+        'students.Student',
+        related_name='reserved_book_items',
+        on_delete=models.DO_NOTHING,
+        null=True,blank=True
+    )
     status= models.CharField(max_length=255)
     purchased_on= models.DateField()
     published_on= models.DateField()
-    rack= models.ForeignKey('racks.Rack',related_name='book_items',on_delete=models.DO_NOTHING)
+    rack= models.ForeignKey(
+        'racks.Rack',
+        related_name='book_items',
+        on_delete=models.DO_NOTHING
+    )
--- book_items/serializers.py
+++ book_items/serializers.py
@@ -4,4 +4,7 @@ from rest_framework import serializers
 class BookItemSerializer(serializers.ModelSerializer):
     class Meta:
         model= BookItem
-        fields= ["id","book","reference","status","purchased_on","published_on","rack"]
+        fields= [
+            "id","book","reference","loaned_to","reserved_by",
+            "status","purchased_on","published_on","rack"
+        ]
